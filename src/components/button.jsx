fe02b4651ff01ec62c4e70e273a3cf155ca8b9dc
Rafaela Sousa Costa <rafaelaengsousa@gmail.com>
2025-08-18 10:05:59 -0300

    Sessão sobre nós

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,13 +1,13 @@
-export function Button({children}) {
+export function Button({ children }) {
   return (
     <a
       href=""
       className="hidden md:inline-flex items-center justify-center h-[56px] w-auto px-6
-                         rounded-full text-white font-poppins font-bold text-xl
-                         bg-gradient-to-b from-[#F54900] to-[#FFB86A]
-                         shadow-[0_4px_10px_0_#F54900]"
+                 rounded-full text-white font-poppins font-bold text-xl
+                 bg-gradient-to-b from-[#F54900] via-[#F54900] via-60% to-[#FFB86A]
+                 shadow-[0_4px_10px_0_#F54900]"
     >
       {children}
     </a>
-  )
+  );
 }
--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -1,26 +1,49 @@
+import { Button } from '../components/button';
+
 export function Sobre() {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-6 text-center px-4 sm:px-6 lg:px-10">
-      {/* Título */}
-      <h2 className="text-8xl font-poppins font-medium text-white">
+    <div className="flex flex-col items-center justify-center text-center px-4 sm:px-6 lg:px-10 mt-20">
+      
+      <h2 className="text-4xl sm:text-5xl md:text-6xl font-poppins font-medium text-white mb-4">
         Sobre nós
       </h2>
 
-      {/* Linha */}
-      <div className="w-[40rem] h-1 rounded shadow-[0px_4px_20px_0px_#F54900] bg-gradient-to-r from-[#F54900] from-10% to-[#FFB86A]" />
+      <div className="w-64 sm:w-96 h-0.5 rounded shadow-[0px_4px_20px_0px_#F54900] bg-gradient-to-r from-[#F54900] to-[#dfaa71] mb-12" />
 
-      {/* Subtítulo em linha */}
-      <p className="text-[32px] text-white font-light">
+      <p className="text-lg sm:text-xl md:text-2xl text-white font-light mb-16 max-w-[70rem]">
         Criada por alunos do ensino médio, a{' '}
         <span className="text-[#F54900] font-semibold">Sportiger</span> nasceu da paixão
         pelo esporte e da admiração pela força e determinação dos atletas.
       </p>
-      <div class="flex gap-8"> 
-        <div> 
-            {/* Imagem */}
+
+     
+      <div className="flex flex-col md:flex-row w-full gap-8  mb-20">
+
+        
+        <div className="w-full md:w-1/2 flex justify-center md:justify-end px-4 md:px-8">
+          <img
+            src="/src/assets/grupo.svg"
+            alt="Imagem dos integrantes da Sportiger"
+            className="w-3/4 md:w-11/12 max-w-[500px] h-auto object-cover"
+          />
         </div>
-        <div>
-            {/* Texto 2 */}
+
+    
+        <div className="w-full md:w-1/2 flex flex-col items-center md:items-start justify-start px-4 md:px-8 mt-8 md:mt-0">
+          <p className="text-3xl sm:text-4xl md:text-[42px] font-light text-white font-poppins mb-4 md:mb-6">
+            Por que Sportiger?
+          </p>
+
+          <p className="text-base sm:text-lg md:text-2xl text-white mt-4 md:mt-6 text-justify mb-10">
+            Nosso nome reflete nossa essência.{' '}
+            <span className="text-[#FFB86A]">“Sport”</span>, do inglês para esporte, representa nossa dedicação ao mundo esportivo.{' '}
+            <span className="text-[#F54900]">“Tiger”</span>, tigre em inglês, simboliza a garra, a agilidade e a força que vemos em cada atleta — a visão de quem nos inspira.
+          </p>
+
+          <Button href="/produtos">
+            Veja nossos produtos
+          </Button>
+
         </div>
       </div>
     </div>
