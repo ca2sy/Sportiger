7dcd30d4b380569b5d108d2d03776efeb64e3bf0
whosbea <bbarreto953@gmail.com>
2025-08-18 19:29:13 -0300

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import './App.css';
 import { Header } from './pages/header';
 import { Home } from './pages/home';
 import { Sobre } from './pages/sobre';
+import Elipse from '../src/assets/Ellipse_1.svg?react';
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#101820] text-white">
+    <div className="relative z-10 min-h-screen bg-[#101820] text-white">
       <Header />
+      <div className="absolute z-20">
+        {/* <Elipse className="w-3 h-3"/> */}
+        <img
+          src="/src/assets/Ellipse_2.svg"
+          alt="isso AI"
+          className="h-[810px] w-[647px]"
+        />
+      </div>
       <Home />
       <Sobre />
     </div>
--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -2,48 +2,44 @@ import { Button } from '../components/button';
 
 export function Sobre() {
   return (
-    <div className="flex flex-col items-center justify-center text-center px-4 sm:px-6 lg:px-10 mt-20">
-      
-      <h2 className="text-4xl sm:text-5xl md:text-6xl font-poppins font-medium text-white mb-4">
+    <div className="mt-20 flex flex-col items-center justify-center rounded-[50px] bg-[#101820]/40 px-4 pt-30 text-center backdrop-blur-md sm:px-6 lg:px-10">
+      <h2 className="font-poppins mb-4 text-4xl font-medium text-white sm:text-5xl md:text-6xl">
         Sobre nós
       </h2>
 
-      <div className="w-64 sm:w-96 h-0.5 rounded shadow-[0px_4px_20px_0px_#F54900] bg-gradient-to-r from-[#F54900] to-[#dfaa71] mb-12" />
+      <div className="mb-12 h-0.5 w-64 rounded bg-gradient-to-r from-[#F54900] to-[#dfaa71] shadow-[0px_4px_20px_0px_#F54900] sm:w-96" />
 
-      <p className="text-lg sm:text-xl md:text-2xl text-white font-light mb-16 max-w-[70rem]">
+      <p className="mb-16 max-w-[70rem] text-lg font-light text-white sm:text-xl md:text-2xl">
         Criada por alunos do ensino médio, a{' '}
-        <span className="text-[#F54900] font-semibold">Sportiger</span> nasceu da paixão
-        pelo esporte e da admiração pela força e determinação dos atletas.
+        <span className="font-semibold text-[#F54900]">Sportiger</span> nasceu
+        da paixão pelo esporte e da admiração pela força e determinação dos
+        atletas.
       </p>
 
-     
-      <div className="flex flex-col md:flex-row w-full gap-8  mb-20">
-
-        
-        <div className="w-full md:w-1/2 flex justify-center md:justify-end px-4 md:px-8">
+      <div className="mb-20 flex w-full flex-col gap-8 md:flex-row">
+        <div className="flex w-full justify-center px-4 md:w-1/2 md:justify-end md:px-8">
           <img
             src="/src/assets/grupo.svg"
             alt="Imagem dos integrantes da Sportiger"
-            className="w-3/4 md:w-11/12 max-w-[500px] h-auto object-cover rounded-sm"
+            className="h-auto w-3/4 max-w-[500px] rounded-sm object-cover md:w-11/12"
           />
         </div>
 
-    
-        <div className="w-full md:w-1/2 flex flex-col items-center md:items-start justify-start px-4 md:px-8 mt-8 md:mt-0">
-          <p className="text-3xl sm:text-4xl md:text-[42px] font-light text-white font-poppins mb-4 md:mb-6">
+        <div className="mt-8 flex w-full flex-col items-center justify-start px-4 md:mt-0 md:w-1/2 md:items-start md:px-8">
+          <p className="font-poppins mb-4 text-3xl font-light text-white sm:text-4xl md:mb-6 md:text-[42px]">
             Por que Sportiger?
           </p>
 
-          <p className="text-base sm:text-lg md:text-2xl text-white mt-4 md:mt-6 text-justify mb-10">
+          <p className="mt-4 mb-10 text-justify text-base text-white sm:text-lg md:mt-6 md:text-2xl">
             Nosso nome reflete nossa essência.{' '}
-            <span className="text-[#FFB86A]">“Sport”</span>, do inglês para esporte, representa nossa dedicação ao mundo esportivo.{' '}
-            <span className="text-[#F54900]">“Tiger”</span>, tigre em inglês, simboliza a garra, a agilidade e a força que vemos em cada atleta — a visão de quem nos inspira.
+            <span className="text-[#FFB86A]">“Sport”</span>, do inglês para
+            esporte, representa nossa dedicação ao mundo esportivo.{' '}
+            <span className="text-[#F54900]">“Tiger”</span>, tigre em inglês,
+            simboliza a garra, a agilidade e a força que vemos em cada atleta —
+            a visão de quem nos inspira.
           </p>
 
-          <Button href="/produtos">
-            Veja nossos produtos
-          </Button>
-
+          <Button href="/produtos">Veja nossos produtos</Button>
         </div>
       </div>
     </div>
