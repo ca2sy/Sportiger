bcd85d3ea7d3504f2bee7ca45dc47a7cbe59c4c5
whosbea <bbarreto953@gmail.com>
2025-08-18 19:40:21 -0300

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,20 @@ import Elipse from '../src/assets/Ellipse_1.svg?react';
 
 function App() {
   return (
-    <div className="relative z-10 min-h-screen bg-[#101820] text-white">
+    <div className="relative z-10 min-h-screen overflow-x-hidden bg-[#101820] text-white">
       <Header />
       <div className="absolute z-20">
-        {/* <Elipse className="w-3 h-3"/> */}
         <img
           src="/src/assets/Ellipse_2.svg"
           alt="isso AI"
           className="h-[810px] w-[647px]"
+        />
+      </div>
+      <div className="absolute top-20 -right-40 z-20">
+        <img
+          src="/src/assets/Ellipse_3.svg"
+          alt="isso AI"
+          className="h-[2000px] w-[1000px]"
         />
       </div>
       <Home />
