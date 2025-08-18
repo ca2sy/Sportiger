256f438104505adcd18d9de95c2eb2018696364c
whosbea <bbarreto953@gmail.com>
2025-08-18 10:21:22 -0300

    component update

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -4,7 +4,7 @@ export function Button({ children }) {
       href=""
       className="hidden md:inline-flex items-center justify-center h-[56px] w-auto px-6
                  rounded-full text-white font-poppins font-bold text-xl
-                 bg-gradient-to-b from-[#F54900] via-[#F54900] via-60% to-[#FFB86A]
+                 bg-gradient-to-b from-[#F54900] via-[#F54900] via-70% to-[#FFB86A]
                  shadow-[0_4px_10px_0_#F54900]"
     >
       {children}
